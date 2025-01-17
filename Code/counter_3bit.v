635afc3ae36070278f7ae0721a46e9ae9b8ed5e7
moenguyenx <qminh1908@gmail.com>
2025-01-17 14:30:13 +0700

    fuck

--- Code/counter_3bit.v
+++ Code/counter_3bit.v
@@ -1,34 +1,39 @@
 module Counter (
-    input clk,
-    input reset_n,
-    input [2:0] count_to,
-    input load,
-    input count_en,
-    output reg done,
-    output reg [2:0] count
+  input clk,
+  input reset_n,
+  input [2:0] count_to,
+  input load,
+  input count_en,
+  output reg done,
+  output reg [2:0] count
 );
 
-    always @(posedge clk or negedge reset_n) begin
-        if (~reset_n) begin
-            // Reset logic
-            count <= 3'b0;
-            done <= 1'b0;
+  reg [2:0] next_count;
+
+  always @(posedge clk or negedge reset_n) begin
+    if (~reset_n) begin
+      // Reset logic
+      count <= 3'b0;
+      done <= 1'b0;
+    end else begin
+      // Counter logic
+      if (load) begin
+        // Load new value
+        next_count = count_to;
+        count <= next_count;
+        done <= 1'b0;
+      end else if (count_en) begin
+        // Count down
+        if (count == 3'b0) begin
+          next_count = 3'b0;
+          done <= 1'b1;
         end else begin
-            if (load) begin
-                // Load new value
-                count <= count_to;
-                done <= 1'b0; // Reset done
-            end else if (count_en) begin
-                if (count == 3'b0) begin
-                    count <= 3'b0; // giữ nguyên 0
-                    done <= 1'b1; // Đặt done = 1
-                end else begin
-                    count <= count - 1; // giảm giá trị
-                    done <= 1'b0; // Reset done
-                end
-            end
+          next_count <= count - 1;
+          done <= 1'b0;
         end
+        count <= next_count;
+      end
     end
+  end
 
 endmodule
-
